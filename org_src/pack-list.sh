9dfbe146661e030174e9f97625dcc622914f141e
cnsnyder <csnyder@redhat.com>
2014-06-18 14:33:58 -0400

    Deletes original perl source

--- org_src/pack-list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-host=`hostname`
-file=$host-packages.txt
-rpm -qa --qf "%{NAME}|%{VERSION}|%{RELEASE}|%{INSTALLTIME}|%{VENDOR}|%{BUILDTIME}|%{BUILDHOST}|%{SOURCERPM}|%{LICENSE}|%{PACKAGER}\n" > $file
-tar -cjf $host.tar.bz2 $file
-echo "please submit $host.tar.bz2"
-rm $file
