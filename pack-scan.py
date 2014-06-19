5b6cfcdf54e1ccd3ada267fa12ef702c18286064
cnsnyder <csnyder@redhat.com>
2014-06-19 18:40:20 -0400

    Adds SystemReport class representing a system report. PackScanCli now only handles arg parsing.

--- pack-scan.py
+++ pack-scan.py
@@ -17,15 +17,27 @@ class PackScanCli(object):
         self.main()
 
     def main(self, args=None):
-        #TODO: Add arg parsing for list of hosts to cover functionality of pack-scan-ssh.pl
-        #TODO: Add file read in functionality
 
-        test = PackScanCmd()
-        test.main()
+        if not args:
+            args = sys.argv[1:]
+
+        self.options, self.args = self.parser.parse_args(args)
+        if hasattr(self.options, "filename") and self.options.filename:
+            PackScanFileCmd(self.options.filename).main()
+        elif hasattr(self.options, "user_host") and self.options.user_host:
+            try:
+                user, host = self.options.user_host
+            except ValueError, e:
+                print 'Invalid input. Please try again.'
+                exit(1)
+            PackScanUserHostCmd(user, host).main()
+        else:
+            PackScanCmd().main()
+
 
     def add_options(self):
         self.parser.add_option("-r", "--remote", help="Executes pack-scan using ssh for the given [USER HOST]", dest="user_host", default=None, nargs=2)
-        self.parser.add_option("-f", "--file", help="File of [USER HOST] to run the tool against.", dest="file")
+        self.parser.add_option("-f", "--file", help="File of [USER HOST] to run the tool against.", dest="filename")
 
 
 # This class contains the information retrieved about and installed package
@@ -100,7 +112,7 @@ class PackScanCmd(object):
     def populate_data(self):
         for var, cmd in self.cmds.iteritems():
             self.exitcode[var], self.cmd_results[var] = self.run_cmd(cmd)
-        self.parse_data()
+        # self.parse_data()
 
     def parse_data(self):
         for key, value in self.cmd_results.iteritems():
@@ -111,56 +123,140 @@ class PackScanCmd(object):
 
     def main(self):
         self.populate_data()
+        PackScanCmd.save_results(SystemReport(self.cmd_results))
 
-        installed_packages = [PkgInfo(line) for line in self.rpm_output.splitlines()]
-        rh_packages = filter(PkgInfo.is_red_hat_pkg, installed_packages)
-        greatest = max(rh_packages, key= lambda x: x.install_time)
-        greatest_build = max(rh_packages, key= lambda x: x.build_time)
-
-        result = "%s (%s)  -  %s" % (self.host, self.release, self.date)
-        result += "\nRed Hat (Y/N), "
-        if (len(rh_packages) > 0):
-            result += "Y"
-            result += "\nRH Pkgs, %s/%s" % (len(rh_packages), len(installed_packages))
-            result += "\nLast Installed, "
-            result += greatest.details_install()
-
-            result += "\nLast Built, %s" % greatest_build.details_built()
-        else:
-            result += "N"
-        if self.uid != '0':
-            result += "\nvirt.is_guest, Not run as root"
-            result += "\nvirt.host_type, Not run as root"
-            result += "\nDmi Info, Not run as root"
-        else:
-            result += "\nVirt-What installed (Y/N), "
-
-            if self.virt_what_path:
-                result += "Y"
-                if self.exitcode['virt_what'] == 0:
-                    if self.virt_what:
-                        # Writes all virt-what facts to the output as a double quoted field.
-                        result += '\nVirt-what Facts, "%s"' % self.virt_what.replace('\n', ', ')
-                    else:
-                        result += '\nVirt Host, None'
-            else:
-                result += "N"
-        self.save_results(result, self.host)
-
-    def save_results(self, output, host):
-        filename = '%s.csv' % host
+    @staticmethod
+    def save_results(report):
+        filename = '%s.csv' % report.host
         try:
             f = open(filename, 'w')
-            f.write(output)
+            f.write(str(report))
             f.close()
         except EnvironmentError, e:
             sys.stderr.write('Error writing to %s: %s\n' % (filename, e))
             f.close()
-        subprocess.call(["zip", "%s.zip" % host, "%s.csv" % host])
-        subprocess.call(["rm", "%s.csv" % host])
+        subprocess.call(["zip", "%s.zip" % report.host, "%s.csv" % report.host])
+        subprocess.call(["rm", "%s.csv" % report.host])
+
+class PackScanUserHostCmd(PackScanCmd):
+    name = "user_host"
+
+    def __init__(self, user, host):
+        super(PackScanUserHostCmd, self).__init__()
+
+        # Host will be passed in in the case of this command and need not be run remotely
+        del self.cmds['host']
+        self.user = user
+        self.cmd_results['host'] = host
 
 
+    def run_cmd(self, cmd):
+        command = "ssh %s@%s '%s'" % (self.user, self.cmd_results['host'], cmd)
+        return commands.getstatusoutput(command)
 
+class PackScanFileCmd(PackScanUserHostCmd):
+    name = "file"
+
+    def __init__(self, filename):
+        self.user_hosts = []
+        self.filename = filename
+        self.parse_file()
+
+    def parse_file(self):
+        try:
+            f = open(self.filename, 'r')
+            self.user_hosts = [line.strip().split(' ') for line in f.readlines()]
+            f.close()
+        except EnvironmentError, e:
+            sys.stderr.write('Error writing to %s: %s\n' % (filename, e))
+            f.close()
+
+    def main(self):
+        [PackScanUserHostCmd(x,y).main() for x,y in self.user_hosts]
+
+
+class SystemReport(object):
+    """The SystemReport class represents a report on a system with the given data"""
+    # This is a dict of fields and functions that will return the appropriate string value for the field
+    not_root_msg = "Not root"
+    def __init__(self, data, seperator=","):
+        self.data = data
+        self.seperator = seperator
+        self.fields = [
+            ["Red Hat (Y/N)" , self.is_red_hat],
+            ["RH Pkgs" , self.rhpkgs],
+            ["Last Installed" , self.last_installed],
+            ["Last Built" , self.last_built],
+            ["Virt Host Info" , self.virt_host],
+            ["Sockets" , self.sockets],
+            ["Cores" , self.cores,]
+        ]
+
+        self.parse_data()
+        self.installed_packages = [PkgInfo(line) for line in self.rpm_output.splitlines()]
+        self.rh_packages = filter(PkgInfo.is_red_hat_pkg, self.installed_packages)
+        self.greatest = max(self.rh_packages, key= lambda x: x.install_time)
+        self.greatest_build = max(self.rh_packages, key= lambda x: x.build_time)
+
+
+    def __str__(self):
+        result = "%s (%s)  -  %s" % (self.host, self.release, self.date)
+        try:
+            for field, value_func in self.fields:
+                result += "\n%s%s %s" % (field, self.seperator, value_func())
+        except NotRedHatException, e:
+            result += "\nRed Hat (Y/N), N"
+
+        return result
+
+    # adds all items in data as attributes of this instance of SystemReport
+    def parse_data(self):
+        for key, value in self.data.iteritems():
+            setattr(self, key, value.strip())
+
+    def is_red_hat(self):
+        if len(self.rh_packages) > 0:
+            return "Y"
+        else:
+            raise NotRedHatException()
+
+    def rhpkgs(self):
+        return "%s/%s" % (len(self.rh_packages), len(self.installed_packages))
+
+    def last_installed(self):
+        return self.greatest.details_install()
+
+    def last_built(self):
+        return self.greatest_build.details_built()
+
+    def virt_host(self):
+        if self.uid !='0':
+            return self.not_root_msg
+        elif hasattr(self, "virt_what_path") and len(self.virt_what_path) == 0:
+            return "virt-what missing"
+        elif self.virt_what:
+            return '"%s"' % self.virt_what.replace('\n', ', ')
+        elif self.virt_what == "":
+            return "Baremetal"
+        else:
+            return "Error running virt-what"
+
+    def sockets(self):
+        if self.uid != '0':
+            return self.not_root_msg
+        else:
+            return len(re.findall('Socket Designation', self.dmiinfo))
+
+    def cores(self):
+        if self.uid != '0':
+            return self.not_root_msg
+        else:
+            core_count_match = re.findall('Core Count: ([0-9]+)', self.dmiinfo)
+            num_cores = sum([int(x) for x in core_count_match])
+            return num_cores
+
+class NotRedHatException(BaseException):
+    pass
 
 if __name__ == '__main__':
     PackScanCli()
